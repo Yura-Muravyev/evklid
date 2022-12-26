2e2badebb8c11be6386ff8cb2401c51511a600cd
Yuriy Muravyev <yura.muravyev@yandex.ru>
2022-12-26 17:49:57 +0300

    Доделать анимацию строки поиска

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const $header = document.querySelector(".header");
+const $search = document.querySelector(".btn-loupe");
+const $closed_search = document.querySelector(".closed__search");
+
+$search.addEventListener("click", () => {
+  document.querySelector(".header").classList.toggle("searh--open");
+});
+
+$closed_search.addEventListener("click", () => {
+  document.querySelector(".header").classList.toggle("searh--open");
+});
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
