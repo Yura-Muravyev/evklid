254071fa58800d55c74c97bbf1cbb95b86584044
Yuriy Muravyev <yura.muravyev@yandex.ru>
2022-12-28 16:55:20 +0300

    хедер готов

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 const $header = document.querySelector(".header");
 const $search = document.querySelector(".btn-loupe");
 const $closed_search = document.querySelector(".closed__search");
+const $header__burger_btn = document.querySelector(".header__burger-btn");
 
-$search.addEventListener("click", () => {
-  document.querySelector(".header").classList.toggle("searh--open");
-});
+// $search.addEventListener("click", () => {
+//   document.querySelector(".searh").classList.remove("display-none");
+//   setTimeout(() => {
+//     document.querySelector(".header").classList.add("searh--open");
+//   }, 300);
+// });
 
-$closed_search.addEventListener("click", () => {
-  document.querySelector(".header").classList.toggle("searh--open");
+// $closed_search.addEventListener("click", () => {
+//   document.querySelector(".header").classList.remove("searh--open");
+//   setTimeout(() => {
+//     document.querySelector(".searh").classList.add("display-none");
+//   }, 200);
+// });
+
+$header__burger_btn.addEventListener("click", () => {
+  document.querySelector(".header").classList.toggle("nav--open");
 });
 
 const swiper = new Swiper(".swiper", {
